a99d6b7b3a2c9279ef6e6e8043dc9935876f4cbe
Hugh Kaznowski <phkaznowski@gmail.com>
2020-03-10 15:25:10 +0000

    Initial commit

---
+++ src/main/java/com/example/gatewayreactiveloadbalancer/GatewayReactiveLoadBalancerApplication.java
@@ -0,0 +1,13 @@
+package com.example.gatewayreactiveloadbalancer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayReactiveLoadBalancerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayReactiveLoadBalancerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gatewayreactiveloadbalancer/GatewayReactiveLoadBalancerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gatewayreactiveloadbalancer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayReactiveLoadBalancerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
