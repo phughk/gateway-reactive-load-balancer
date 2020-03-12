8c0a01eef9b2025cc3f796c7db43f39eeeba70ba
Hugh Kaznowski <phkaznowski@gmail.com>
2020-03-12 11:01:01 +0000

    Not working discovery for service one

--- version-one/src/main/java/com/kaznowski/versionone/VersionOne.java
+++ version-one/src/main/java/com/kaznowski/versionone/VersionOne.java
@@ -2,15 +2,18 @@ package com.kaznowski.versionone;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class VersionOne {
   public static void main(String[] args) {
     SpringApplication.run(VersionOne.class, args);
   }
 
-  @RequestMapping("version")
+  @GetMapping("/v1.0/version")
   String user() {
     return "Version 1";
   }
