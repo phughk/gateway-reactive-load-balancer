85f593768d77e788600594f1e0f0ab410a526ca6
Alfred Mountfield <alfred.mountfield@student.manchester.ac.uk>
2020-03-16 13:49:39 +0000

    Added bean extraction in the Spring Main class to demonstrate instances aren't being found through the discovery client

--- gateway/src/main/java/com/example/gatewayreactiveloadbalancer/GatewayReactiveLoadBalancerApplication.java
+++ gateway/src/main/java/com/example/gatewayreactiveloadbalancer/GatewayReactiveLoadBalancerApplication.java
@@ -2,12 +2,22 @@ package com.example.gatewayreactiveloadbalancer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EurekaDiscoveryClient;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class GatewayReactiveLoadBalancerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(GatewayReactiveLoadBalancerApplication.class, args);
+		ConfigurableApplicationContext ctx = SpringApplication.run(GatewayReactiveLoadBalancerApplication.class, args);
+		EurekaDiscoveryClient discoveryClient = (EurekaDiscoveryClient) ctx.getBean("discoveryClient");
+
+		// prints [version-one]
+		System.out.println(discoveryClient.getServices());
+		// prints []
+		System.out.println(discoveryClient.getInstances("version-one"));
 	}
 
 }
